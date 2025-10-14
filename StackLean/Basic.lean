9bbfb4b1926fc339e0aed5975387065d8b29a0ee
clonker <1685266+clonker@users.noreply.github.com>
2025-10-14 21:07:20 +0200

    Dependent types for Stack: Stack and stack ops now depend on length

--- StackLean/Basic.lean
+++ StackLean/Basic.lean
@@ -31,10 +31,14 @@ def LSet.ofList [DecidableEq α] : (vs : List α) → LSet α
       let rest := LSet.ofList tl
       if helem : hd ∈ rest
       then rest
-      else ⟨hd :: rest.val, by
-        simp ; apply And.intro
-        · trivial
-        · apply rest.property
+      else
+      ⟨
+        hd :: rest.val,
+        by
+          simp ;
+          apply And.intro
+          · trivial
+          · apply rest.property
       ⟩
 
 --- Stack Slots ---
@@ -55,34 +59,35 @@ instance : LawfulBEq Slot := inferInstance
 
 -- Stacks ---
 
-abbrev Stack := List Slot
+abbrev Stack (sz : Nat):= { l : List Slot // l.length = sz}
 
-def Stack.vars (s : Stack) : LSet Var :=
+
+def Stack.vars (s : Stack sz) : LSet Var :=
   let unwrap := λ s =>
     match s with
       | .Var v => some v
       | _ => none
 
-  s |> List.filterMap unwrap |> LSet.ofList
+  s.val |> List.filterMap unwrap |> LSet.ofList
 
 --- Targets ---
 
 -- A target is a specification of a desired shuffle outcome
-structure Target where
+structure Target (nargs : Nat) where
   -- a concrete args section that must have a specific order and multiplicity
-  args : Stack
+  args : Stack nargs
   -- an abstract tail section that is a set of variables that must remain live for downstream ops
   liveOut : LSet Var
 
-def Target.surplus (target : Target) (stack : Stack) (slot : Slot) : Prop :=
-  let target_args_count := target.args.count slot
-  let stack_head_count := stack |> List.take target.args.length |> List.count slot
+def Target.surplus (target : Target nargs) (stack : Stack sz) (slot : Slot) : Prop :=
+  let target_args_count := target.args.val.count slot
+  let stack_head_count := stack.val |> List.take target.args.val.length |> List.count slot
   match slot with
   | .Var v => target_args_count < stack_head_count ∧ v ∈ target.liveOut
   | .Lit _ => target_args_count < stack_head_count
   | .Junk => True
 
-instance (target : Target) (stack : Stack) (slot : Slot) : Decidable (target.surplus stack slot) := by
+instance (target : Target nargs) (stack : Stack sz) (slot : Slot) : Decidable (target.surplus stack slot) := by
   unfold Target.surplus
   match slot with
   | .Var v => exact inferInstance
@@ -91,83 +96,80 @@ instance (target : Target) (stack : Stack) (slot : Slot) : Decidable (target.sur
 
 --- Stack Manipulation ---
 
-inductive Op where
+-- The operations that can be applied to a stack of a given size
+inductive Op : Nat → Type where
   -- swap0 is a noop, the others match the evm
-  | Swap     : Fin 17 → Op
+  | Swap : (_ : szin > 0) → Fin (min 17 szin) → Op szin
   -- dupn => evm dup(n + 1)
-  | Dup      : Fin 16 → Op
-  | Pop      : Op
-  | Push     : Word → Op
-  | MarkJunk : Fin 1024 → Op
+  | Dup : (_ : szin > 0) → Fin (min 16 szin) → Op szin
+  | Pop : (_ : szin > 0) → Op szin
+  | Push : Word → Op szin
+  | MarkJunk : Fin szin → Op szin
 
-inductive Err where
-  | StackTooDeep : Err
-  | StackTooShallow : Err
+-- The effect an operation has on the size of a stack
+@[reducible, simp]
+def sizeAfter (_ : Stack szin) (op : Op szin) : Nat := match op with
+  | .Swap _ _ => szin
+  | .Dup _ _ => szin + 1
+  | .Pop _ => szin - 1
+  | .Push _ => szin + 1
+  | .MarkJunk _ => szin
 
-def Stack.apply (stack : Stack) : Op → Except Err Stack
-  | .Swap idx =>
-      if h : idx < stack.length
-      then
-        let s := stack.set 0 stack[idx]
-        return s.set (idx) stack[0]
-      else throw .StackTooShallow
-  | .Dup idx =>
-      if h : idx < stack.length
-      then return (stack[idx] :: stack)
-      else throw .StackTooShallow
-  | .Pop =>
-      if stack.length > 0
-      then return stack.tail
-      else throw .StackTooShallow
-  | .Push v => return (.Lit v :: stack)
-  | .MarkJunk idx =>
-      if idx < stack.length
-      then return stack.set idx .Junk
-      else throw .StackTooShallow
+def Stack.apply (stack : Stack szin) (op : Op szin) : Stack (sizeAfter stack op) :=
+  have : stack.val.length = szin := stack.property
+  match op with
+    | .Swap _ idx =>
+        have : idx < stack.val.length := by omega
+
+        let s := stack.val.set 0 stack.val[idx]
+        have : s.length = stack.val.length := by simp_all only [Fin.getElem_fin, List.length_set, s]
+
+        ⟨s.set (idx) stack.val[0], by simp_all⟩
+    | .Dup _ idx => ⟨stack.val[idx] :: stack.val, by simp_all⟩
+    | .Pop _ => ⟨stack.val.tail, by simp_all⟩
+    | .Push val => ⟨.Lit val :: stack.val, by simp_all⟩
+    | .MarkJunk idx =>
+      have : idx < stack.val.length := by omega
+      ⟨stack.val.set idx .Junk, by simp_all⟩
 
 -- apply theorems --
 
 -- applying swap at the same idx twice is a noop
-theorem apply_swap_inv (stack : Stack) (idx : Fin 17) (h : idx < stack.length) :
-  stack.apply (.Swap idx) >>= (·.apply (.Swap idx)) = .ok stack := by
-    simp_all [Stack.apply, Bind.bind, Except.bind, pure, Except.pure, List.length_set]
-    ext i
-    simp [List.getElem_set]
+theorem apply_swap_inv (sz : Nat) (hsz : sz > 0) (stack : Stack sz) (idx : Fin (min 17 sz)):
+  (stack.apply (.Swap hsz idx)).apply (.Swap hsz idx) = stack := by
+    simp_all [Stack.apply, List.getElem_set]
     split_ifs
     · simp_all
-    · simp_all only [List.getElem?_set]
+    · ext i
+      simp_all [List.getElem?_set]
       split_ifs <;> simp_all
 
 -- swap does not change the length of the stack
-theorem apply_swap_length (stack : Stack) (idx : Fin 17) (h : idx < stack.length) :
-  List.length <$> stack.apply (.Swap idx) = .ok stack.length := by
-    simp_all [Stack.apply, pure, Except.pure, List.length_set]
+theorem apply_swap_length (sz : Nat) (hsz : sz > 0) (stack : Stack sz) (idx : Fin (min 17 sz)) :
+  (stack.apply (.Swap hsz idx)).val.length = sz := by
+    simp [Stack.apply, stack.property]
 
 -- swap switches the elem at idx with the elem at 0
-theorem apply_swap_positions_switched (stack : Stack) (idx : Fin 17) (h : idx < stack.length) :
-  ∃ (s' : Stack), stack.apply (.Swap idx) = .ok s' ∧ s'[0]? = some stack[idx] ∧ s'[idx]? = some stack[0] := by
-  simp_all [Stack.apply, pure, Except.pure, List.length_set, List.getElem?_set]
-  split_ifs <;> simp_all
+theorem apply_swap_positions_switched (sz: Nat) (hsz : sz > 0) (stack : Stack sz) (idx : Fin (min 17 sz)) :
+  ∃ (s' : Stack sz), stack.apply (.Swap hsz idx) = s' ∧ s'.val[0] = stack.val[idx] ∧ s'.val[idx]? = some stack.val[0] := by
+  have : idx < stack.val.length := by omega
+  simp_all [Stack.apply, List.length_set, List.getElem_set]
 
 -- dup grows stack size by 1
-theorem apply_dup_grows_stack (stack : Stack) (idx : Fin 16) (h : idx < stack.length) :
-  (stack.apply (.Dup idx) >>= (pure $ ·.length)) = .ok (stack.length + 1) := by
-    simp_all [Stack.apply, Bind.bind, Except.bind, pure, Except.pure]
+theorem apply_dup_grows_stack (sz : Nat) (hsz: sz > 0) (stack : Stack sz) (idx : Fin (min 16 sz)) :
+  (stack.apply (.Dup hsz idx)).val.length = (stack.val.length + 1) := by simp [Stack.apply]
 
 -- the stack head is the same as the element at idx after applying (dup idx)
-theorem apply_dup_stack_top_eq_idx (stack : Stack) (idx : Fin 16) (h : idx < stack.length) :
-  (stack.apply (.Dup idx) >>= (pure $ ·[0]?)) = .ok (stack[idx]?) := by
-    simp_all [Stack.apply, Bind.bind, Except.bind, pure, Except.pure]
+theorem apply_dup_stack_top_eq_idx (sz : Nat) (hsz: sz > 0) (stack : Stack sz) (idx : Fin (min 16 sz)) :
+  (stack.apply (.Dup hsz idx)).val[0]'(by simp [Stack.apply]) = (stack.val[idx]) := by simp [Stack.apply]
 
 -- the tail of the new stack is the same as the starting stack after applying (dup idx)
-theorem apply_dup_stack_tail_eq_prev (stack : Stack) (idx : Fin 16) (h : idx < stack.length) :
-  (stack.apply (.Dup idx) >>= (pure $ ·.tail)) = .ok (stack) := by
-    simp_all [Stack.apply, Bind.bind, Except.bind, pure, Except.pure]
+theorem apply_dup_stack_tail_eq_prev (sz : Nat) (hsz : sz > 0) (stack : Stack sz) (idx : Fin (min 16 sz)) :
+  (stack.apply (.Dup hsz idx)).val.tail = stack := by simp [Stack.apply]
 
 -- the result of applying pop is the same as the tail of the input stack
-theorem apply_pop_eq_prev_stack_tail (stack : Stack) (h : stack.length > 0) :
-  stack.apply .Pop = .ok (stack.tail) := by
-    simp_all [Stack.apply, pure, Except.pure]
+theorem apply_pop_eq_prev_stack_tail (sz : Nat) (hsz : sz > 0) (stack : Stack sz) :
+  stack.apply (.Pop hsz) = stack.val.tail := by simp [Stack.apply]
 
 -- TODO: push / markjunk theorems
 
